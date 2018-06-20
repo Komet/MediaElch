c1ce9f2f7e6010c1a389ea1cbcb6f31f912a2e04
Andre Meyering <info@andremeyering.de>
2018-06-20 12:51:43 +0200

    [TvShow] Refactor tree view

--- smallWidgets/TvShowTreeView.h
+++ smallWidgets/TvShowTreeView.h
@@ -17,9 +17,18 @@ protected:
     void drawRow(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
 private:
-    QPixmap m_newIcon;
-    QPixmap m_syncIcon;
-    QPixmap m_missingIcon;
+    void drawTvShowRow(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void drawEpisodeRow(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void drawRowBackground(QPainter *painter, QStyleOptionViewItem option, const QModelIndex &index) const;
+    int getTvShowType(const QModelIndex &index) const;
+
+    const QPixmap m_newIcon;
+    const QPixmap m_syncIcon;
+    const QPixmap m_missingIcon;
+
+    const int m_seasonIndent = 30;
+    const int m_episodeIndent = 50;
+    const int m_branchIndent = 30;
 };
 
 #endif // TVSHOWTREEVIEW_H
