ddbb08c44a875509d81b6d4d48567961e6f68f1c
Andre Meyering <info@andremeyering.de>
2018-06-30 13:01:26 +0200

    [Scripts] Update root/script path in bash scripts

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [ ! -f MediaElch.pro ]; then
-	echo ""
-	echo "    Call \"./scripts/format.sh\" from MediaElch root directory!"
-	echo ""
-	exit 1
-fi
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-# Format all files using clang-format
+pushd "$SCRIPT_PATH/.." > /dev/null
+
+echo "Format all files using clang-format"
 find . ! -path "*/quazip/*" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i -style=file {} \;
+
+popd > /dev/null
