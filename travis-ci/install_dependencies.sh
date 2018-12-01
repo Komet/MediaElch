ce1c77dc5503bde0787d8b4a676555661cdeb6ed
Andre Meyering <info@andremeyering.de>
2018-12-01 17:50:48 +0100

    [CI] Use xenial dist for Windows build

--- travis-ci/install_dependencies.sh
+++ travis-ci/install_dependencies.sh
@@ -88,7 +88,7 @@ if [ $(lc "${OS_NAME}") = "linux" ]; then
 		fold_start "mediainfodll"
 		print_info "Downloading MediaInfoDLL"
 		wget --output-document MediaInfoDLL.7z https://mediaarea.net/download/binary/libmediainfo0/${MEDIAINFO_VERSION}/MediaInfo_DLL_${MEDIAINFO_VERSION}_Windows_i386_WithoutInstaller.7z
-		7z x -oMediaInfo MediaInfoDLL.7z
+		7zr x -oMediaInfo MediaInfoDLL.7z
 		mv MediaInfo/Developers/Source/MediaInfoDLL ./MediaInfoDLL
 		mv MediaInfo/MediaInfo.dll MediaInfo.dll
 		rm -rf MediaInfo MediaInfoDLL.7z
