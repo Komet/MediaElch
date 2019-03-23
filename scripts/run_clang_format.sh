7bcab2f3ac7efdf2abdfb61aa231b52109bccfbc
Andre Meyering <info@andremeyering.de>
2019-03-23 14:01:24 +0100

    [Scripts] Bundle clang-format calls

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPT_PATH/.."
 
 echo "Format all source files using clang-format"
-find src -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i -style=file {} \;
+find src -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i -style=file {} \+
 
 echo "Format all test files using clang-format"
-find test -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i -style=file {} \;
+find test -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i -style=file {} \+
