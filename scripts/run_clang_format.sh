2379ae21580988f00f11c9b2ef3202a444dcf254
Andre Meyering <info@andremeyering.de>
2019-07-17 17:14:23 +0200

    [Scripts] Add check for clang-format version 7
     - that's the version used in travis-ci

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -7,6 +7,8 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd "$SCRIPT_PATH/.."
 
+clang-format --version | grep " 7." > /dev/null || ( echo "WARNING: MediaElch requires clang-format version 7")
+
 echo "Format all source files using clang-format"
 find src -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i -style=file {} \+
 
