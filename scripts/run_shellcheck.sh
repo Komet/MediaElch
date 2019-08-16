f362a0a5ad2a4d1a057acbabd42ad858071ab9a3
Andre Meyering <info@andremeyering.de>
2019-08-16 18:12:18 +0200

    [Scripts] Ignore "ls|grep" shellcheck check

--- scripts/run_shellcheck.sh
+++ scripts/run_shellcheck.sh
@@ -15,12 +15,13 @@ print_important "Run shellcheck on all source files"
 
 find . ! -path "./build/*" -type f -name "*.sh" \
 	-exec shellcheck -x \
-	-e SC2086,SC1090,SC2143,SC1091 \
+	-e SC2086,SC1090,SC2143,SC1091,SC2010 \
 	{} \+
 
 # SC2086: Double quote to prevent globbing and word splitting.
 # SC1090: Can't follow non-constant source. Use a directive to specify location
 # SC2143: Use grep -q instead of comparing output with
 # SC1091: Not following
+# SC2010: Don't use ls | grep. Use a glob or a for loop with a condition to allow non-alphanumeric filenames.
 
 print_success "No issues found! Great! :-)"
