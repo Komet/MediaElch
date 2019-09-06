845682be4f182f7cacaf7aaa5bf8143950e37874
Andre Meyering <info@andremeyering.de>
2019-09-06 19:54:00 +0200

    [Kodi] Add ArtistXmlWriter for Kodi v16/v17/v18

---
+++ src/media_centers/kodi/v18/ArtistXmlWriterV18.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "media_centers/kodi/v17/ArtistXmlWriterV17.h"
+
+namespace mediaelch {
+namespace kodi {
+
+// currently the same
+using ArtistXmlWriterV18 = ArtistXmlWriterV17;
+
+} // namespace kodi
+} // namespace mediaelch
