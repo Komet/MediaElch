4e7b8f10af5bf1fbbdab0c89bb0c627df03edcea
Andre Meyering <info@andremeyering.de>
2019-10-16 16:03:59 +0200

    [CMake] Also check for AppleClang in colors.cmake

--- cmake/colors.cmake
+++ cmake/colors.cmake
@@ -3,7 +3,8 @@
 if(${ENABLE_COLOR_OUTPUT})
   if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
     add_compile_options(-fdiagnostics-color=always)
-  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
+    OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
     add_compile_options(-fcolor-diagnostics)
   endif()
 endif()
