Komet/MediaElch

commit e1936dcd1848daed2172f35ee16eb4e4dba05d64

[TMDb] Load movie collection details
 - not used in the UI, yet

src/scrapers/movie/TMDb.cpp
@@ -520,10 +520,8 @@ void TMDb::loadData(QMap<MovieScraperInterface*, QString> ids, Movie* movie, QVe
     movie->controller()->setLoadsLeft(loadsLeft);
 }
 
-/**
- * @brief Called when the movie infos are downloaded
- * @see TMDb::parseAndAssignInfos
- */
+/// Called when the movie infos are downloaded
+/// @see TMDb::parseAndAssignInfos
 void TMDb::loadFinished()
 {
     auto* reply = dynamic_cast<QNetworkReply*>(QObject::sender());
@@ -537,10 +535,67 @@ void TMDb::loadFinished()
     if (reply->error() == QNetworkReply::NoError) {
         const QString msg = QString::fromUtf8(reply->readAll());
         parseAndAssignInfos(msg, movie, infos);
+
+        // if the movie is part of a collection then download the collection data
+        // and delay the call to removeFromLoadsLeft(ScraperData::Infos)
+        // to loadCollectionFinished()
+        if (infos.contains(MovieScraperInfos::Set)) {
+            loadCollection(movie, movie->set().tmdbId);
+            return;
+        }
+
     } else {
         qWarning() << "Network Error (load)" << reply->errorString();
     }
+
     movie->controller()->removeFromLoadsLeft(ScraperData::Infos);
+}
+
+void TMDb::loadCollection(Movie* movie, const TmdbId& collectionTmdbId)
+{
+    if (!collectionTmdbId.isValid()) {
+        return;
+    }
+
+    QNetworkRequest request;
+    request.setRawHeader("Accept", "application/json");
+    request.setUrl(getCollectionUrl(collectionTmdbId.toString()));
+
+    QNetworkReply* const reply = m_qnam.get(request);
+    new NetworkReplyWatcher(this, reply);
+    reply->setProperty("storage", Storage::toVariant(reply, movie));
+    connect(reply, &QNetworkReply::finished, this, &TMDb::loadCollectionFinished);
+}
+
+void TMDb::loadCollectionFinished()
+{
+    auto* reply = dynamic_cast<QNetworkReply*>(QObject::sender());
+    Movie* const movie = reply->property("storage").value<Storage*>()->movie();
+    reply->deleteLater();
+    if (movie == nullptr) {
+        return;
+    }
+
+    const QString msg = QString::fromUtf8(reply->readAll());
+    QJsonParseError parseError{};
+    const auto parsedJson = QJsonDocument::fromJson(msg.toUtf8(), &parseError).object();
+    if (parseError.error != QJsonParseError::NoError) {
+        qWarning() << "Error parsing info json " << parseError.errorString();
+        return;
+    }
+
+    movie->controller()->removeFromLoadsLeft(ScraperData::Infos);
+
+    if (parsedJson.keys().contains("success") && !parsedJson.value("success").toBool()) {
+        qWarning() << "[TMDb] Error message from TMDb:" << parsedJson.value("status_message");
+        return;
+    }
+
+    MovieSet set;
+    set.tmdbId = TmdbId(parsedJson.value("id").toInt());
+    set.name = parsedJson.value("name").toString();
+    set.overview = parsedJson.value("overview").toString();
+    movie->setSet(set);
 }
 
 /**
@@ -701,6 +756,18 @@ QUrl TMDb::getMovieUrl(QString movieId, ApiMovieDetails type, const UrlParameter
     return QUrl{url.append(queries.toString())};
 }
 
+/// @brief Get the collection URL for TMDb. Adds the API key.
+QUrl TMDb::getCollectionUrl(QString collectionId) const
+{
+    auto url = QStringLiteral("https://api.themoviedb.org/3/collection/%1?").arg(collectionId);
+
+    QUrlQuery queries;
+    queries.addQueryItem("api_key", TMDb::apiKey());
+    queries.addQueryItem("language", localeForTMDb());
+
+    return QUrl{url.append(queries.toString())};
+}
+
 /**
  * @brief Parses JSON data and assigns it to the given movie object
  *        Handles all types of data from TMDb (info, releases, trailers, casts, images)
@@ -737,6 +804,7 @@ void TMDb::parseAndAssignInfos(QString json, Movie* movie, QVector<MovieScraperI
     if (infos.contains(MovieScraperInfos::Set) && parsedJson.value("belongs_to_collection").isObject()) {
         const auto collection = parsedJson.value("belongs_to_collection").toObject();
         MovieSet set;
+        set.tmdbId = TmdbId(collection.value("id").toInt());
         set.name = collection.value("name").toString();
         movie->setSet(set);
     }

src/media_centers/kodi/MovieXmlReader.cpp
@@ -144,13 +144,13 @@ void MovieXmlReader::movieSet(const QDomElement& movieSetElement)
     // We need to support both the old and new XML syntax.
     //
     // New Kodi v17 XML Syntax:
-    // <set>
-    //   <name>Movie Set Name</name>
-    //   <overview></overview>
-    // </set>
+    //   <set>
+    //     <name>Movie Set Name</name>
+    //     <overview>movie collection overview</overview>
+    //   </set>
     //
     // Old Syntax:
-    // <set>Movie Set Name</set>
+    //   <set>Movie Set Name</set>
     //
     MovieSet set;
     if (!setNameElements.isEmpty()) {

src/media_centers/kodi/v17/MovieXmlWriterV17.cpp
@@ -126,7 +126,7 @@ QByteArray MovieXmlWriterV17::getMovieXml()
 
     // <set>
     //   <name>...</name>
-    //   <overview></overview>
+    //   <overview>...</overview>
     // </set>
     KodiXml::removeChildNodes(doc, "set");
     if (!m_movie.set().name.isEmpty()) {

src/movies/MovieSet.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "data/TmdbId.h"
+
 #include <QString>
 
+/// Represents a movie collection (aka. set).
 struct MovieSet
 {
+    /// A collection's TmdbId, e.g. 1241 for Harry Potter.
+    /// Used for getting data from TMDb, e.g.
+    /// themoviedb.org/movie/1241 which redirects to
+    /// themoviedb.org/collection/1241-harry-potter-collection
+    TmdbId tmdbId{TmdbId::NoId};
     QString name;
     QString overview;
 };

src/scrapers/movie/TMDb.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "data/TmdbId.h"
+#include "scrapers/movie/MovieScraperInterface.h"
+
 #include <QComboBox>
 #include <QLocale>
 #include <QMap>
@@ -8,8 +11,6 @@
 #include <QPointer>
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtNetwork/QNetworkReply>
-
-#include "scrapers/movie/MovieScraperInterface.h"
 
 /**
  * @brief The TMDb class
@@ -45,6 +46,7 @@ signals:
 private slots:
     void searchFinished();
     void loadFinished();
+    void loadCollectionFinished();
     void loadCastsFinished();
     void loadTrailersFinished();
     void loadImagesFinished();
@@ -81,9 +83,14 @@ private:
     QString localeForTMDb() const;
     QString language() const;
     QString country() const;
+
     QString apiUrlParameterString(ApiUrlParameter parameter) const;
     QUrl getMovieSearchUrl(const QString& searchStr, const UrlParameterMap& parameters) const;
     QUrl
     getMovieUrl(QString movieId, ApiMovieDetails type, const UrlParameterMap& parameters = UrlParameterMap{}) const;
+    QUrl getCollectionUrl(QString collectionId) const;
+
     void parseAndAssignInfos(QString json, Movie* movie, QVector<MovieScraperInfos> infos);
+    /// Load the given collection (TMDb id) and store the content in the movie.
+    void loadCollection(Movie* movie, const TmdbId& collectionTmdbId);
 };

test/integration/media_centers/testKodi_v16_movie.cpp
@@ -92,9 +92,11 @@ TEST_CASE("Movie XML writer for Kodi v16", "[data][movie][kodi][nfo]")
         movie.addGenre("Adventure");
         movie.addGenre("Science Fiction");
         movie.addCountry("United States of America");
+
         MovieSet set;
         set.name = "Divergent Collection";
         movie.setSet(set);
+
         movie.addTag("Best Tag");
         movie.setDirector("Robert Schwentke");
         movie.setWriter("Adam Cooper, Bill Collage, Stephen Chbosky");

test/integration/media_centers/testKodi_v18_movie.cpp
@@ -145,9 +145,18 @@ TEST_CASE("Movie XML writer for Kodi v18", "[data][movie][kodi][nfo]")
         movie.addGenre("Adventure");
         movie.addGenre("Science Fiction");
         movie.addCountry("United States of America");
+
         MovieSet set;
+        set.tmdbId = TmdbId(283579);
         set.name = "Divergent Collection";
+        set.overview =
+            "A series of dystopian science fiction action films based on the Divergent novels by the American author "
+            "Veronica Roth. Set in a dystopian and post-apocalyptic Chicago where people are divided into distinct "
+            "factions based on human virtues. Beatrice Prior (Tris) is warned that she is Divergent and thus will "
+            "never fit into any one of the factions. She along with Tobias Eaton (Four) soon learn that a sinister "
+            "plot is brewing in the seemingly perfect society.";
         movie.setSet(set);
+
         movie.addTag("Best Tag");
         movie.setDirector("Robert Schwentke");
         movie.setWriter("Adam Cooper, Bill Collage, Stephen Chbosky");

test/resources/movie/kodi_v18_movie_all.nfo
@@ -4,11 +4,11 @@
     <originaltitle>Allegiant</originaltitle>
     <sorttitle>TmovieFc10</sorttitle>
     <ratings>
-        <rating default="true" name="imdb" max="10">
+        <rating default="true" max="10" name="imdb">
             <value>5.8</value>
             <votes>1641</votes>
         </rating>
-        <rating default="false" name="themoviedb" max="10">
+        <rating default="false" max="10" name="themoviedb">
             <value>1.2</value>
             <votes>3400</votes>
         </rating>
@@ -34,7 +34,7 @@
     <country>United States of America</country>
     <set>
         <name>Divergent Collection</name>
-        <overview></overview>
+        <overview>A series of dystopian science fiction action films based on the Divergent novels by the American author Veronica Roth. Set in a dystopian and post-apocalyptic Chicago where people are divided into distinct factions based on human virtues. Beatrice Prior (Tris) is warned that she is Divergent and thus will never fit into any one of the factions. She along with Tobias Eaton (Four) soon learn that a sinister plot is brewing in the seemingly perfect society.</overview>
     </set>
     <credits>Adam Cooper</credits>
     <credits>Bill Collage</credits>

commit 353211a036791365ab4788971c47a88176c530ac

[TMDb] Fix missing scraperLoadDone event for movies without sets
 - bug introduced in e1936dcd1848daed2172f35ee16eb4e4dba05d64

src/scrapers/movie/TMDb.cpp
@@ -550,6 +550,7 @@ void TMDb::loadFinished()
 void TMDb::loadCollection(Movie* movie, const TmdbId& collectionTmdbId)
 {
     if (!collectionTmdbId.isValid()) {
+        movie->controller()->removeFromLoadsLeft(ScraperData::Infos);
         return;
     }
 