9971f1b35c78005d274f201b5470ee928a75a0f5
Andre Meyering <info@andremeyering.de>
2020-02-08 11:10:44 +0100

    [Shellcheck] Disable warning about "cd .."

--- scripts/run_shellcheck.sh
+++ scripts/run_shellcheck.sh
@@ -15,7 +15,7 @@ print_important "Run shellcheck on all source files"
 
 find . ! -path "./build/*" -type f -name "*.sh" \
 	-exec shellcheck -x \
-	-e SC2086,SC1090,SC2143,SC1091,SC2010 \
+	-e SC2086,SC1090,SC2143,SC1091,SC2010,SC2103 \
 	{} \+
 
 # SC2086: Double quote to prevent globbing and word splitting.
@@ -23,5 +23,6 @@ find . ! -path "./build/*" -type f -name "*.sh" \
 # SC2143: Use grep -q instead of comparing output with
 # SC1091: Not following
 # SC2010: Don't use ls | grep. Use a glob or a for loop with a condition to allow non-alphanumeric filenames.
+# SC2103: Use a ( subshell ) to avoid having to cd back.
 
 print_success "No issues found! Great! :-)"
