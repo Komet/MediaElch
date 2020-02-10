dc26a45000dd9d97bdda5df5eeff941088121317
Andre Meyering <info@andremeyering.de>
2020-02-10 20:20:21 +0100

    [Test] Re-enable IMDB id test; add QDebug tests

--- test/unit/data/testImdbId.cpp
+++ test/unit/data/testImdbId.cpp
@@ -2,6 +2,8 @@
 
 #include "data/ImdbId.h"
 
+#include <sstream>
+
 TEST_CASE("ImdbId data type", "[data]")
 {
     SECTION("Default Case")
@@ -12,10 +14,33 @@ TEST_CASE("ImdbId data type", "[data]")
         CHECK(ImdbId("") == ImdbId::NoId);
         CHECK(ImdbId() == ImdbId::NoId);
     }
+
     SECTION("Correct IMDb format")
     {
         CHECK(ImdbId("tt1234567").isValid());
         CHECK(ImdbId::isValidFormat("tt1234567"));
     }
+
     SECTION("Conversion") { CHECK(ImdbId("tt1234567").toString() == "tt1234567"); }
+
+    SECTION("output stream")
+    {
+        std::stringstream stream;
+
+        stream << ImdbId("tt1234567") << ';';
+        stream << ImdbId() << ';';
+
+        CHECK(stream.str() == "tt1234567;;");
+    }
+
+    SECTION("QDebug output")
+    {
+        QString buffer;
+        QDebug stream(&buffer);
+
+        stream << ImdbId("tt1234567");
+        stream << ImdbId();
+
+        CHECK(buffer == "ImdbId(\"tt1234567\") ImdbId(\"\") ");
+    }
 }
