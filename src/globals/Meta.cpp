90519d139b29b2085a77600723b5011c6b56020f
Andre Meyering <info@andremeyering.de>
2020-04-08 18:03:33 +0200

    [Globals] Add Meta file for C++ meta functions
    Qt 5.5 does not support QOverload so we have to add it ourselves.
    To be removed when Qt 5.6 is required.

---
+++ src/globals/Meta.cpp
@@ -0,0 +1,3 @@
+#include "globals/Meta.h"
+
+// Empty
---
+++ src/globals/Meta.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+// Partially copied from Qt: qtbase/src/corelib/global/qglobal.h
+// Qt 5.5 does not support Overload so we have to provide it ourselves.
+// This code can be removed when Qt 5.6 is required.
+//
+// Copyright (C) 2019 The Qt Company Ltd.
+// Modified to avoid name clashes.
+
+template<typename... Args>
+struct NonConstOverload
+{
+    template<typename R, typename T>
+    constexpr auto operator()(R (T::*ptr)(Args...)) const noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+    template<typename R, typename T>
+    static constexpr auto of(R (T::*ptr)(Args...)) noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+};
+
+template<typename... Args>
+struct ConstOverload
+{
+    template<typename R, typename T>
+    constexpr auto operator()(R (T::*ptr)(Args...) const) const noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+    template<typename R, typename T>
+    static constexpr auto of(R (T::*ptr)(Args...) const) noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+};
+
+template<typename... Args>
+struct Overload : ConstOverload<Args...>, NonConstOverload<Args...>
+{
+    using ConstOverload<Args...>::of;
+    using ConstOverload<Args...>::operator();
+    using NonConstOverload<Args...>::of;
+    using NonConstOverload<Args...>::operator();
+    template<typename R>
+    constexpr auto operator()(R (*ptr)(Args...)) const noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+    template<typename R>
+    static constexpr auto of(R (*ptr)(Args...)) noexcept -> decltype(ptr)
+    {
+        return ptr;
+    }
+};
+
+template<typename... Args>
+constexpr Overload<Args...> elchOverload = {};
+template<typename... Args>
+constexpr ConstOverload<Args...> elchConstOverload = {};
+template<typename... Args>
+constexpr NonConstOverload<Args...> elchNonConstOverload = {};
