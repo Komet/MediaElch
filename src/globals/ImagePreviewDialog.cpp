9e00494b5d82ea752bd8a2557227e1a32d234393
Andre Meyering <info@andremeyering.de>
2020-04-11 20:17:35 +0200

    [ImageDialog] Fix position of image preview in main window
    The code passed global coordinates to "move" but it expected relative
    ones to its parent widget.

--- src/globals/ImagePreviewDialog.cpp
+++ src/globals/ImagePreviewDialog.cpp
@@ -1,19 +1,17 @@
 #include "ImagePreviewDialog.h"
 #include "ui_ImagePreviewDialog.h"
 
+#include "globals/Helper.h"
+
 #include <QDebug>
 #include <QScrollBar>
 
-#include "globals/Globals.h"
-#include "globals/Helper.h"
-
-/**
- * @brief ImagePreviewDialog::ImagePreviewDialog
- */
 ImagePreviewDialog::ImagePreviewDialog(QWidget* parent) : QDialog(parent), ui(new Ui::ImagePreviewDialog)
 {
     ui->setupUi(this);
+
     connect(ui->buttonClose, &QAbstractButton::clicked, this, &QDialog::accept);
+
 #ifdef Q_OS_MAC
     setWindowFlags((windowFlags() & ~Qt::WindowType_Mask) | Qt::Sheet);
     setStyleSheet(styleSheet() + " #ImagePreviewDialog { border: 1px solid rgba(0, 0, 0, 100); border-top: none; }");
@@ -22,19 +20,11 @@ ImagePreviewDialog::ImagePreviewDialog(QWidget* parent) : QDialog(parent), ui(ne
 #endif
 }
 
-/**
- * @brief ImagePreviewDialog::~ImagePreviewDialog
- */
 ImagePreviewDialog::~ImagePreviewDialog()
 {
     delete ui;
 }
 
-/**
- * @brief Returns an instance of the dialog
- * @param parent Parent widget
- * @return Instance of ImagePreviewDialog
- */
 ImagePreviewDialog* ImagePreviewDialog::instance(QWidget* parent)
 {
     static ImagePreviewDialog* s_instance = nullptr;
@@ -44,34 +34,27 @@ ImagePreviewDialog* ImagePreviewDialog::instance(QWidget* parent)
     return s_instance;
 }
 
-/**
- * @brief Sets the image to be displayed
- * @param img Image
- */
 void ImagePreviewDialog::setImage(QPixmap img)
 {
-    qDebug() << "Entered";
     helper::setDevicePixelRatio(img, helper::devicePixelRatio(this));
     ui->image->setPixmap(img);
 }
 
-/**
- * @brief Executes the dialog and adjusts its size
- * @return The QDialog::exec result
- */
 int ImagePreviewDialog::exec()
 {
-    qDebug() << "Entered";
+    qDebug() << "[ImagePreviewDialog] Open image preview dialog";
+
     ui->scrollArea->verticalScrollBar()->setValue(0);
     ui->scrollArea->horizontalScrollBar()->setValue(0);
+
     QSize newSize;
     newSize.setHeight(parentWidget()->size().height() - 50);
     newSize.setWidth(qMin(1200, parentWidget()->size().width() - 100));
     resize(newSize);
 
+    // move assumes coordinates relative to the parent widget
     int xMove = (parentWidget()->size().width() - size().width()) / 2;
-    QPoint globalPos = parentWidget()->mapToGlobal(parentWidget()->pos());
-    move(globalPos.x() + xMove, globalPos.y());
+    move(parentWidget()->x() + xMove, parentWidget()->y());
 
     return QDialog::exec();
 }
