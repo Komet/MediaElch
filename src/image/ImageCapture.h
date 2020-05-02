e73bd366f8a4497d31222ab33325fadbf4d37b24
Andre Meyering <info@andremeyering.de>
2020-05-02 14:56:01 +0200

    [Movie] Allow screenshot for movie backdrop
    Just like for posters, allow taking screenshots for backdrops.

--- src/image/ImageCapture.h
+++ src/image/ImageCapture.h
@@ -17,11 +17,11 @@ public:
     /// @brief Captures a screenshot of a given video file at a random time.
     ///
     /// Resizes it to the given dimension with respect to its aspect ratio.
+    /// If one dimension is 0 then no scaling is performed at all.
     /// If cropFromCenter is true then the image will be resized to *exactly*
     /// the given dimensions and will crop a rectangle from the screenshot's
     /// center. Otherwise the resulting image may be smaller in size or height
     /// than the given dimensions.
-    ///
     static bool captureImage(FilePath file,
         StreamDetails* streamDetails,
         ThumbnailDimensions dim,
