bfb63bc24f7cbf42e125c0b8172af187deefef00
Andre Meyering <info@andremeyering.de>
2020-06-08 12:35:43 +0200

    [network] Add WebsiteCache module
    This module can be used to cache html pages.

---
+++ src/network/WebsiteCache.cpp
@@ -0,0 +1,65 @@
+#include "network/WebsiteCache.h"
+
+#include <QDateTime>
+#include <QString>
+#include <QUrl>
+
+namespace mediaelch {
+namespace scraper {
+
+WebsiteCache::WebsiteCache()
+{
+    QObject::connect(&m_timer, &QTimer::timeout, [this]() { clearOldCacheEntries(); });
+}
+
+bool WebsiteCache::hasValidElement(const QUrl& url, const Locale& locale)
+{
+    const QString h = hash(url, locale);
+    return m_cache.contains(h) && m_cache[h].date >= QDateTime::currentDateTime().addSecs(-timeoutSeconds);
+}
+
+QString WebsiteCache::hash(const QUrl& url, const Locale& locale)
+{
+    return QStringLiteral("%1_##_%2").arg(locale.toString(), url.toString());
+}
+
+void WebsiteCache::addElement(const QUrl& url, const Locale& locale, QString data)
+{
+    if (data.isEmpty() || !url.isValid()) {
+        return;
+    }
+    CacheElement c;
+    c.data = std::move(data);
+    c.date = QDateTime::currentDateTime();
+    m_cache.insert(hash(url, locale), c);
+
+    if (!m_timer.isActive()) {
+        // set timer for clearing the cache
+        m_timer.start(timeoutSeconds * 1000);
+    }
+}
+
+QString WebsiteCache::getElement(const QUrl& url, const Locale& locale)
+{
+    const QString h = hash(url, locale);
+    return m_cache[h].data;
+}
+
+void WebsiteCache::clearOldCacheEntries()
+{
+    auto it = m_cache.begin();
+    while (it != m_cache.end()) {
+        if (it.value().date >= QDateTime::currentDateTime().addSecs(-timeoutSeconds)) {
+            it = m_cache.erase(it);
+        } else {
+            ++it;
+        }
+    }
+
+    if (!m_cache.isEmpty()) {
+        m_timer.start(timeoutSeconds * 1000);
+    }
+}
+
+} // namespace scraper
+} // namespace mediaelch
---
+++ src/network/WebsiteCache.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "data/Locale.h"
+
+#include <QDateTime>
+#include <QMap>
+#include <QString>
+#include <QTimer>
+#include <QUrl>
+
+namespace mediaelch {
+namespace scraper {
+
+/// \brief TheTvDb API cache stores the result for a API request as a string.
+///
+/// Clears cache elements every timeoutSeconds. The cache is *not*
+/// thread safe.
+class WebsiteCache
+{
+public:
+    constexpr static int timeoutSeconds = 240;
+
+    WebsiteCache();
+
+    void addElement(const QUrl& url, const Locale& locale, QString data);
+    QString getElement(const QUrl& url, const Locale& locale);
+    bool hasValidElement(const QUrl& url, const Locale& locale);
+
+private:
+    struct CacheElement
+    {
+        QDateTime date;
+        QString data;
+    };
+
+    QString hash(const QUrl& url, const Locale& locale);
+
+    /// \brief Clears old cache entries that are older than timeoutSeconds.
+    ///
+    /// Restarts the timer if the cache is not empty to ensure that all elements
+    /// are eventually deleted.
+    void clearOldCacheEntries();
+
+    QMap<QUrl, CacheElement> m_cache;
+    QTimer m_timer;
+};
+
+} // namespace scraper
+} // namespace mediaelch
