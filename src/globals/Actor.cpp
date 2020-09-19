ce91f2d1e70a2a0907d6218d836183b17ff4056f
Andre Meyering <info@andremeyering.de>
2020-09-19 15:35:14 +0200

    [Actor] Add qDebug output

--- src/globals/Actor.cpp
+++ src/globals/Actor.cpp
@@ -1,3 +1,16 @@
 #include "globals/Actor.h"
 
-// no-op
+
+QDebug operator<<(QDebug dbg, const Actor& actor)
+{
+    QString nl = "\n";
+    QString out;
+    out.append("Actor").append(nl);
+    out.append(QString("  Name:  ").append(actor.name).append(nl));
+    out.append(QString("  Role:  ").append(actor.role).append(nl));
+    out.append(QString("  Thumb: ").append(actor.thumb).append(nl));
+    out.append(QString("  ID:    ").append(actor.id).append(nl));
+    out.append(QString("  Order: ").append(actor.order).append(nl));
+    dbg.nospace() << out;
+    return dbg.maybeSpace();
+}
