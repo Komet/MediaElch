d69831f1d34c9d17279a33d3d2f3b9f59b222002
Andre Meyering <andre.meyering@sap.com>
2020-12-13 16:47:07 +0100

    [Version] Fix version name

--- Version.h
+++ Version.h
@@ -11,7 +11,7 @@ namespace constants {
 constexpr char AppName[] = "MediaElch";
 constexpr char AppVersionStr[] = "2.8.0";     // major.minor.patch
 constexpr char AppVersionFullStr[] = "2.8.0"; // major.minor.patch-identifier
-constexpr char VersionName[] = "Ferenginar";
+constexpr char VersionName[] = "Coridian";
 constexpr char OrganizationName[] = "kvibes";
 
 #ifdef QT_NO_DEBUG
