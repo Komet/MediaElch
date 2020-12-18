e07647c8e53f4b8915c481aa6f6e11da0ea61907
Andre Meyering <info@andremeyering.de>
2020-12-18 13:22:49 +0100

    [ScraperError] Add function to create a scraper error from network error

--- src/scrapers/ScraperError.cpp
+++ src/scrapers/ScraperError.cpp
@@ -1,5 +1,54 @@
 #include "scrapers/ScraperError.h"
 
+#include <QObject>
+
 namespace mediaelch {
-// no-op
+
+ScraperError networkErrorToScraperError(const QNetworkReply& reply)
+{
+    if (reply.error() == QNetworkReply::NoError) {
+        return {};
+    }
+
+    const auto httpStatusCode = HttpStatusCode(reply.attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt());
+    ScraperError error;
+
+    if (reply.error() != QNetworkReply::UnknownServerError) {
+        error.error = ScraperError::Type::NetworkError;
+        error.message = mediaelch::translateNetworkError(reply.error());
+        error.technical = reply.errorString();
+        return error;
+    }
+
+    if (httpStatusCode == HttpStatusCode::TooManyRequests) {
+        error.error = ScraperError::Type::ApiRateLimitReached;
+        error.message = QObject::tr("The scraper's rate limit reached. Please wait ~10 seconds and try again.");
+        return error;
+    }
+
+    error.error = ScraperError::Type::NetworkError;
+    error.message = QObject::tr("An unknown network error occurred");
+    error.technical = reply.errorString();
+
+    return error;
 }
+
+ScraperError makeScraperError(const QString& data, const QNetworkReply& reply, const QJsonParseError& parseError)
+{
+    ScraperError error;
+    if (reply.error() != QNetworkReply::NoError) {
+        return networkErrorToScraperError(reply);
+
+    } else if (data.isEmpty()) {
+        error.error = ScraperError::Type::ApiError;
+        error.message = QObject::tr("The scraper did not respond with any data.");
+
+    } else if (parseError.error != QJsonParseError::NoError) {
+        error.error = ScraperError::Type::ApiError;
+        error.message = QObject::tr("The scraper response could not be parsed.");
+        error.technical = parseError.errorString();
+    }
+
+    return error;
+}
+} // namespace mediaelch
--- src/scrapers/ScraperError.h
+++ src/scrapers/ScraperError.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "network/HttpStatusCodes.h"
+
+#include <QJsonParseError>
+#include <QNetworkReply>
 #include <QString>
 
 // TODO: Add namespace mediaelch when all scrapers are inside this namespace.
@@ -23,7 +27,23 @@ struct ScraperError
         ApiUnauthorized
     };
     Type error = Type::NoError;
+    /// \brief User-readable error message. Should be translated.
     QString message;
+    /// \brief Technical error, used for debug output.
+    QString technical;
 
     bool hasError() const { return (error != Type::NoError); }
 };
+
+namespace mediaelch {
+
+/// \brief A utility function to create a scraper error object based on common API steps.
+/// \details Most scrapers have a similar setup:  They use JSON as a response and may have
+///          a rate limit.  This function checks all those cases and creates a scraper error
+///          with default messages, etc.
+ScraperError makeScraperError(const QString& data, const QNetworkReply& reply, const QJsonParseError& parseError);
+
+/// \brief A utility function to create a scraper error object based on a network reply.
+ScraperError networkErrorToScraperError(const QNetworkReply& reply);
+
+} // namespace mediaelch
