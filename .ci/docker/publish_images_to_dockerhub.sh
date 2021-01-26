e0804158aa83c6a4b6b9134a0fed8b74bf3f5c33
Andre Meyering <info@andremeyering.de>
2021-01-26 14:36:16 +0100

    [Docker] Add images for use in Jenkins

---
+++ .ci/docker/publish_images_to_dockerhub.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+###########################################################
+# This script releases docker images to Docker Hub.
+# These images are used for our CI pipelines, e.g. Jenkins.
+###########################################################
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Change to this directory
+cd "$(dirname "$(realpath -s "$0")")"
+
+docker build -t mediaelch/mediaelch-ci-linux:latest -f Dockerfile.ci.linux .
+docker push mediaelch/mediaelch-ci-linux:latest
+
+# Note: As of 2021-01-26, this fails.
+# mxe has an open issue for qtbase, see <https://github.com/mxe/mxe/issues/2590>
+docker build -t mediaelch/mediaelch-ci-win:latest -f Dockerfile.ci.windows .
+docker push mediaelch/mediaelch-ci-win:latest
