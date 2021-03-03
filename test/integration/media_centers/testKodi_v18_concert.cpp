5238c0e124348527a49e13c041724783f7fb1999
Andre Meyering <info@andremeyering.de>
2021-03-03 14:22:39 +0100

    [Concert] Use QXmlStreamReader instead of DOM based parsing

--- test/integration/media_centers/testKodi_v18_concert.cpp
+++ test/integration/media_centers/testKodi_v18_concert.cpp
@@ -23,9 +23,8 @@ static void createAndCompareConcert(const QString& filename, Callback callback)
     QString concertContent = getFileContent(filename);
 
     mediaelch::kodi::ConcertXmlReader reader(concert);
-    QDomDocument doc;
-    doc.setContent(concertContent);
-    reader.parseNfoDom(doc);
+    QXmlStreamReader stream(concertContent);
+    reader.parse(stream);
 
     callback(concert);
 
@@ -49,18 +48,22 @@ TEST_CASE("Concert XML writer for Kodi v18", "[data][concert][kodi][nfo]")
         checkSameXml(getFileContent(filename), actual);
     }
 
-    // TODO: Deactivated due to too many changes
-    // SECTION("read / write details: Rammstein in Amerika 2015")
-    // {
-    //     createAndCompareConcert("concert/kodi_v18_Rammstein_in_Amerika_2015.nfo", [](Concert& concert) {
-    //         // check some details
-    //         CHECK(concert.name() == "Rammstein in Amerika");
-    //         REQUIRE(!concert.ratings().isEmpty());
-    //         CHECK(concert.ratings().first().voteCount == 7653);
-    //         CHECK(concert.image(ImageType::ConcertPoster).size() == 176);  // TODO: currently every thumb is a
-    //         poster... CHECK(concert.image(ImageType::ConcertBackdrop).size() == 57); // <fanart>
-    //         CHECK(concert.certification() == Certification("Rated R"));
-    //         CHECK(concert.artist() == "Rammstein");
-    //     });
-    // }
+    SECTION("read / write details: Rammstein in Amerika 2015")
+    {
+        createAndCompareConcert("concert/kodi_v18_Rammstein_in_Amerika_2015.nfo", [](Concert& concert) {
+            // check some details
+            CHECK(concert.title() == "Rammstein in Amerika");
+            CHECK(concert.originalTitle() == "RAMMSTEIN in Amerika");
+            REQUIRE(!concert.ratings().isEmpty());
+            CHECK(concert.ratings().first().voteCount == 15);
+            // TODO: Difference to posters()?
+            // CHECK(concert.image(ImageType::ConcertPoster).size() == 176);  // TODO: currently every thumb is a poster...
+            CHECK(concert.posters().size() == 4);
+            // TODO: Difference to backdrops()?
+            // CHECK(concert.image(ImageType::ConcertBackdrop).size() == 57); // <fanart>
+            CHECK(concert.backdrops().size() == 5);
+            CHECK(concert.certification() == Certification("Rated R"));
+            CHECK(concert.artist() == "Rammstein");
+        });
+    }
 }
