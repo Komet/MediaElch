4e7dc9d8a5c780401c817faf9c0f725bed733d30
Andre Meyering <info@andremeyering.de>
2021-03-28 12:26:23 +0200

    [CI] Fix sha512 sums for ffmpeg versions (Linux/Windows)
    Last commit accidentally used the Linux shasum for Windows.

--- .ci/third_party_versions.sh
+++ .ci/third_party_versions.sh
@@ -10,6 +10,7 @@ export MAC_CREATE_DMG_GIT_HASH=fbe0f36c823adbcbdcc15f9d65c6354252ac8307
 export WIN_MEDIAINFO_VERSION="20.03"
 # From https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
 export WIN_FFMPEG_VERSION="ffmpeg-4.3.2-2021-02-27-essentials_build"
-export WIN_FFMPEG_SHA512="d03d31378c3225d39a90ff43f612041c0e90ed4e8196b7074f7908fbb4d1b820efdee4c08bdcd334c1ea626c6c25a3dd19183ebcfd7535c727ff67d1cccb8b65  ffmpeg.zip"
+export WIN_FFMPEG_SHA512="2e0a8ebf671b2ed3c13e47f7b3cdf02f45ad95c85b768531572c024b29a60a01a1947ecfdc769033fd704329af627b63df631827c4d83a1c4effc75397af73eb  ffmpeg.zip"
 
-export LINUX_FFMPEG_SHA512="4629887efe26e1473636639e19b42e040217089bbeefd4ca059234d64d9b8ae1e8f6b5b925eeda012ea0665af0fcbb9f68f667a34aa696e38a3644c2bc3fbf16  ffmpeg.tar.xz" # 4.3.1
+# From https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+export LINUX_FFMPEG_SHA512="d03d31378c3225d39a90ff43f612041c0e90ed4e8196b7074f7908fbb4d1b820efdee4c08bdcd334c1ea626c6c25a3dd19183ebcfd7535c727ff67d1cccb8b65  ffmpeg.tar.xz" # 4.3.1
