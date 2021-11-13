9d88811a649000057e384befa56471614edcac05
Andre Meyering <info@andremeyering.de>
2021-11-13 16:12:00 +0100

    [Scripts] Update to clang-format-12

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -8,20 +8,20 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null 2>&1
 
 source scripts/utils.sh
 
-if [[ -x "$(command -v clang-format-11)" ]]; then
-	CF=clang-format-11
-elif [[ -x "$(command -v clang-format-mp-11)" ]]; then
+if [[ -x "$(command -v clang-format-12)" ]]; then
+	CF=clang-format-12
+elif [[ -x "$(command -v clang-format-mp-12)" ]]; then
 	# MacPorts version
-	CF=clang-format-mp-11
+	CF=clang-format-mp-12
 else
 	CF=clang-format
-	clang-format --version | grep " 11." > /dev/null || (print_warning "WARNING: MediaElch requires clang-format version 11")
+	clang-format --version | grep " 12." > /dev/null || (print_warning "WARNING: MediaElch requires clang-format version 12")
 fi
 
 print_important "Format all source files using ${CF}"
-find src -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec ${CF} -i -style=file {} \+
+find ./src -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec ${CF} -i -style=file {} \+
 
 print_important "Format all test files using ${CF}"
-find test -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec ${CF} -i -style=file {} \+
+find ./test -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec ${CF} -i -style=file {} \+
 
 print_success "Done"
