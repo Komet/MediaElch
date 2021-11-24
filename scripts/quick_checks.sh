011f9e9280b82b6bc3712065dbdcafacaa8d2262
Andre Meyering <info@andremeyering.de>
2021-11-24 17:34:04 +0100

    [Scripts] Remove usage of shfmt
    I no longer see it as important. :-)

--- scripts/quick_checks.sh
+++ scripts/quick_checks.sh
@@ -13,10 +13,10 @@ source ./utils.sh
 
 print_important "Run all 5 quick checks"
 
-print_info "\nRunning check 1 / 5 : Clang Format"
+print_info "\nRunning check 1 / 4 : Clang Format"
 ./run_clang_format.sh
 
-print_info "\nRunning check 2 / 5 : Correct debug macros"
+print_info "\nRunning check 2 / 4 : Correct debug macros"
 if grep -r 'qInfo' ../src; then
 	print_error "Found usages of qInfo! Use qCInfo(category) instead!"
 elif grep -r 'qDebug' ../src; then
@@ -29,14 +29,10 @@ else
 	print_success "Done."
 fi
 
-print_info "\nRunning check 3 / 5 : CMake Format"
+print_info "\nRunning check 3 / 4 : CMake Format"
 ./run_cmake_format.sh
 
-print_info "\nRunning check 4 / 5 : Shellcheck"
+print_info "\nRunning check 4 / 4 : Shellcheck"
 ./run_shellcheck.sh
-
-print_info "\nRunning check 5 / 5 : shfmt"
-print_important "Running shfmt"
-find . -type f -iname '*.sh' -exec shfmt -l -w {} \+
 
 print_success "\nAll checks finished successfully!"
