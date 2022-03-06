dd3306de2114d51862375df8392b5cdbbfc3c15d
Andre Meyering <info@andremeyering.de>
2022-03-06 21:50:13 +0100

    [Scripts] Update to clang-format-13

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -8,14 +8,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null 2>&1
 
 source scripts/utils.sh
 
-if [[ -x "$(command -v clang-format-12)" ]]; then
-	CF=clang-format-12
-elif [[ -x "$(command -v clang-format-mp-12)" ]]; then
+if [[ -x "$(command -v clang-format-13)" ]]; then
+	CF=clang-format-13
+elif [[ -x "$(command -v clang-format-mp-13)" ]]; then
 	# MacPorts version
-	CF=clang-format-mp-12
+	CF=clang-format-mp-13
 else
 	CF=clang-format
-	clang-format --version | grep " 12." > /dev/null || (print_warning "WARNING: MediaElch requires clang-format version 12")
+	clang-format --version | grep " 13." > /dev/null || (print_warning "WARNING: MediaElch requires clang-format version 13")
 fi
 
 print_important "Format all source files using ${CF}"
