88b3609af5f5a6eeccdbd4bac122619e4c205ad9
Andre Meyering <info@andremeyering.de>
2022-03-06 14:26:25 +0100

    [CI] Split up nightly deployment scripts

---
+++ .ci/jenkins/NightlyDeploymentLinux.groovy
@@ -0,0 +1,62 @@
+pipeline {
+
+  agent {
+    docker {
+      image 'mediaelch/mediaelch-ci-appimage:latest'
+      alwaysPull true
+    }
+  }
+
+  options {
+    ansiColor('xterm')
+  }
+
+  triggers {
+    // Each day between 0am and 6am.
+    cron 'H H(0-6) * * *'
+  }
+
+  stages {
+    stage('Build Linux') {
+      steps {
+        // Note: /bin/sh is used, i.e. no Bash
+        sh '''
+          set +e
+          . /opt/qt512/bin/qt512-env.sh
+          set -e
+          ./.ci/linux/build_linux_release.sh --no-confirm
+          '''
+      }
+    }
+    stage('Package AppImage') {
+      steps {
+        // Note: /bin/sh is used, i.e. no Bash
+        sh '''
+          set +e
+          . /opt/qt512/bin/qt512-env.sh
+          set -e
+          ./.ci/linux/package_linux_appimage.sh --no-confirm
+          '''
+      }
+    }
+    stage('Upload AppImage') {
+      steps {
+        withCredentials([sshUserPrivateKey(usernameVariable: 'ssh_user', credentialsId: "mediaelch-downloads-ssh", keyFileVariable: 'keyfile')]) {
+          sh '''
+            if [ ! -d "${HOME}/.ssh" ] || ! grep 'ameyering.de' "${HOME}/.ssh/known_hosts" > /dev/null; then
+              mkdir -p "${HOME}/.ssh"
+              ssh-keyscan -H ameyering.de >> "${HOME}/.ssh/known_hosts"
+            fi
+            scp -i "${keyfile}" *.AppImage ${ssh_user}@ameyering.de:compose_server/mediaelch_downloads/lighttpd_htdocs/snapshots/Linux/
+            '''
+        }
+      }
+    }
+  }
+
+  post {
+    cleanup {
+      sh 'rm -f *.AppImage'
+    }
+  }
+}
---
+++ .ci/jenkins/NightlyDeploymentWindows.groovy
@@ -0,0 +1,50 @@
+pipeline {
+
+  agent {
+    docker {
+      image 'mediaelch/mediaelch-ci-win:latest'
+      alwaysPull true
+    }
+  }
+
+  options {
+    ansiColor('xterm')
+  }
+
+  triggers {
+    // Each day between 0am and 6am.
+    cron 'H H(0-6) * * *'
+  }
+
+  stages {
+    stage('Build Windows MXE') {
+      steps {
+        sh './.ci/win/build_windows_release.sh --no-confirm'
+      }
+    }
+    stage('Package ZIP') {
+      steps {
+        sh './.ci/win/package_windows.sh'
+      }
+    }
+    stage('Upload ZIP') {
+      steps {
+        withCredentials([sshUserPrivateKey(usernameVariable: 'ssh_user', credentialsId: 'mediaelch-downloads-ssh', keyFileVariable: 'keyfile')]) {
+          sh '''
+            if [ ! -d "${HOME}/.ssh" ] || ! grep 'ameyering.de' "${HOME}/.ssh/known_hosts" > /dev/null; then
+              mkdir -p "${HOME}/.ssh"
+              ssh-keyscan -H ameyering.de >> "${HOME}/.ssh/known_hosts"
+            fi
+            scp -i "${keyfile}" *.zip ${ssh_user}@ameyering.de:compose_server/mediaelch_downloads/lighttpd_htdocs/snapshots/Windows/
+            '''
+        }
+      }
+    }
+  }
+
+  post {
+    cleanup {
+      sh 'rm -f *.zip'
+    }
+  }
+}
