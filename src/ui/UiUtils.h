e7decd232d8429a96b847b9b29cb820bf16df70f
Andre Meyering <info@andremeyering.de>
2022-09-24 15:55:59 +0200

    [UI] Refactor: Move setButtonStyle()/ButtonStyle to ui/
    This commit refactors Helper.h: It should not depend on Qt::Widgets.

--- src/ui/UiUtils.h
+++ src/ui/UiUtils.h
@@ -5,10 +5,20 @@
 //       For example create a custom combobox instead of fillStereoModeCombo().
 
 #include <QComboBox>
+#include <QPushButton>
 #include <QWidget>
 
 namespace mediaelch {
 namespace ui {
+
+enum ButtonStyle
+{
+    ButtonPrimary,
+    ButtonInfo,
+    ButtonDanger,
+    ButtonSuccess,
+    ButtonWarning
+};
 
 void fillStereoModeCombo(QComboBox* box);
 
@@ -16,6 +26,8 @@ void removeFocusRect(QWidget* widget);
 
 void applyStyle(QWidget* widget, bool removeFocus = true, bool isTable = false);
 void applyEffect(QWidget* parent);
+
+void setButtonStyle(QPushButton* button, ButtonStyle style);
 
 } // namespace ui
 } // namespace mediaelch
