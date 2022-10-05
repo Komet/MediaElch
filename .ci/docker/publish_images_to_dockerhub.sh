f0fa800829693ddfb761458d40c210843fe07355
Andre Meyering <info@andremeyering.de>
2022-10-05 17:46:01 +0200

    chore(docker ci): Update mold linker version for CI docker image

--- .ci/docker/publish_images_to_dockerhub.sh
+++ .ci/docker/publish_images_to_dockerhub.sh
@@ -3,6 +3,9 @@
 ###########################################################
 # This script releases docker images to Docker Hub.
 # These images are used for our CI pipelines, e.g. Jenkins.
+#
+# NOTE:  We now have Jenkins jobs that do this.
+#        See ../jenkins/BuildDockerImages.groovy
 ###########################################################
 
 set -euo pipefail
@@ -13,6 +16,11 @@ cd "$(dirname "$(realpath -s "$0")")"
 
 docker build --pull -t mediaelch/mediaelch-ci-linux:latest -f Dockerfile.ci.linux .
 docker push mediaelch/mediaelch-ci-linux:latest
+
+
+docker build --pull -t mediaelch/mediaelch-ci-appimage:latest:latest -f Dockerfile.build-ubuntu-16.04 .
+docker push mediaelch/mediaelch-ci-linux:latest
+
 
 # Note: As of 2021-01-26, this may fail.
 # mxe has an open issue for qtbase, see <https://github.com/mxe/mxe/issues/2590>
