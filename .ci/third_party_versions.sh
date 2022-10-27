81ac99fa3749cacb7202a8f64a30c0f175fe6079
Andre Meyering <info@andremeyering.de>
2022-10-27 13:31:33 +0200

    ci(dependencies): Update ffmpeg/MediaInfo for our nightly builds

--- .ci/third_party_versions.sh
+++ .ci/third_party_versions.sh
@@ -5,16 +5,20 @@
 ###########################################################
 # macOS
 
-export MAC_MEDIAINFO_VERSION="21.09"
-export MAC_MEDIAINFO_URL="https://mediaarea.net/download/binary/libmediainfo0/${MAC_MEDIAINFO_VERSION}/MediaInfo_DLL_${MAC_MEDIAINFO_VERSION}_Mac_x86_64+arm64.tar.bz2"
-export MAC_MEDIAINFO_SHA512="763acedfdf6b62c52199d70c5a1332ed93bba70553946493925bd164e0f1c64b4f8cd6c2645230da0c334e6f77ee55dd72f18037c8d89d9f42683e1586365c3a"
+# From https://mediaarea.net/download/binary/libmediainfo0/${VERSION}/MediaInfo_DLL_${VERSION}_Mac_x86_64+arm64.tar.bz2
+# Mirror at https://files.ameyering.de/binaries/macOS/mediainfo/
+export MAC_MEDIAINFO_VERSION="22.09"
+export MAC_MEDIAINFO_URL="https://files.ameyering.de/binaries/macOS/mediainfo/MediaInfo_DLL_${MAC_MEDIAINFO_VERSION}_Mac_x86_64+arm64.tar.bz2"
+export MAC_MEDIAINFO_SHA512="645bdc16f469d6bbc2e476366dd670b9effb29e1ac83141c8dd1c7e1b5e50b66fb0f5eb799f3a9c244f049522a9f69ad4ee2315baf456acf8e53183b7c7e98ef"
 
-export MAC_FFMPEG_VERSION="5.0"
-export MAC_FFMPEG_URL="https://evermeet.cx/ffmpeg/ffmpeg-${MAC_FFMPEG_VERSION}.7z"
-export MAC_FFMPEG_SHA512="ddac583241bb75ae0cb9b48be9dfff399f3d80679e3fc096b911a3e784b2453e880ab9d84c762ffc78f8b5f318d970cc3d2c83078b75cc110f080a4263ac820c"
+# From https://evermeet.cx/ffmpeg/
+# Mirror at https://files.ameyering.de/binaries/macOS/ffmpeg/
+export MAC_FFMPEG_VERSION="5.1.2"
+export MAC_FFMPEG_URL="https://files.ameyering.de/binaries/macOS/ffmpeg/ffmpeg-${MAC_FFMPEG_VERSION}.7z"
+export MAC_FFMPEG_SHA512="e9dc121daae28e2176d80cc2a3b632aa6413b8532677a3093220fca6aba8420250e318e3ebd1fd44c8e77df235c57fa51264bf38666ee5b6165dcc2428e82883"
 
 export MAC_CREATE_DMG_GIT_REPO="https://github.com/andreyvit/create-dmg.git"
-export MAC_CREATE_DMG_GIT_HASH="11a1b1c93cc1af6bcafc36dfad80ff41e789e65f"
+export MAC_CREATE_DMG_GIT_HASH="412e99352bacef0f05f9abe6cc4348a627b7ac56" # 2022-07-20
 
 ###########################################################
 # Windows
@@ -25,7 +29,7 @@ export WIN_MEDIAINFO_SHA512="0cd336d9f5788ca879ddb70235a65efabefd44db996627e2576
 
 # From https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip
 # Mirror at https://files.ameyering.de/binaries/Windows/ffmpeg/
-export WIN_FFMPEG_VERSION="5.0"
+export WIN_FFMPEG_VERSION="5.1.2"
 export WIN_FFMPEG_ARCHIVE_NAME="ffmpeg-${WIN_FFMPEG_VERSION}-essentials_build.zip"
 export WIN_FFMPEG_FOLDER_NAME="ffmpeg-${WIN_FFMPEG_VERSION}-essentials_build"
 export WIN_FFMPEG_URL="https://files.ameyering.de/binaries/Windows/ffmpeg/${WIN_FFMPEG_ARCHIVE_NAME}"
@@ -36,7 +40,7 @@ export WIN_FFMPEG_SHA512="5d85285aa5b3772b1635e837bd7a7366440b8f53f81c70a299c1f7
 
 # From https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 # Mirror at https://files.ameyering.de/binaries/Linux/ffmpeg/
-export LINUX_FFMPEG_VERSION="5.0"
+export LINUX_FFMPEG_VERSION="5.1.1"
 export LINUX_FFMPEG_ARCHIVE_NAME="ffmpeg-${LINUX_FFMPEG_VERSION}-amd64-static.tar.xz"
 export LINUX_FFMPEG_FOLDER_NAME="ffmpeg-${LINUX_FFMPEG_VERSION}-amd64-static"
 export LINUX_FFMPEG_URL="https://files.ameyering.de/binaries/Linux/ffmpeg/${LINUX_FFMPEG_ARCHIVE_NAME}"
