0a440dd1b7762a4ca926704141f6b5342d760ddf
Andre Meyering <info@andremeyering.de>
2022-12-28 18:59:15 +0100

    build: Fix Qt5 build: No implicit const char* -> QVariant conversion
    Qt5 does not implicitly convert `const char*` to a QVariant.  This
    commit fixes it.

--- src/ui/UiUtils.cpp
+++ src/ui/UiUtils.cpp
@@ -1,16 +1,19 @@
 #include "ui/UiUtils.h"
 
+#include <QString>
+#include <QVariant>
+
 namespace mediaelch {
 namespace ui {
 
 void setButtonStyle(QPushButton* button, ButtonStyle style)
 {
     switch (style) {
-    case ButtonStyle::Danger: button->setProperty("buttonstyle", "danger"); break;
-    case ButtonStyle::Primary: button->setProperty("buttonstyle", "primary"); break;
-    case ButtonStyle::Info: button->setProperty("buttonstyle", "info"); break;
-    case ButtonStyle::Warning: button->setProperty("buttonstyle", "warning"); break;
-    case ButtonStyle::Success: button->setProperty("buttonstyle", "success"); break;
+    case ButtonStyle::Danger: button->setProperty("buttonstyle", QStringLiteral("danger")); break;
+    case ButtonStyle::Primary: button->setProperty("buttonstyle", QStringLiteral("primary")); break;
+    case ButtonStyle::Info: button->setProperty("buttonstyle", QStringLiteral("info")); break;
+    case ButtonStyle::Warning: button->setProperty("buttonstyle", QStringLiteral("warning")); break;
+    case ButtonStyle::Success: button->setProperty("buttonstyle", QStringLiteral("success")); break;
     }
 }
 
