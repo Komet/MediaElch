3bb31d830def6ad6b39ee73bbf6c3f4e9be1d78e
Andre Meyering <info@andremeyering.de>
2023-04-30 12:45:11 +0200

    build: Fix build issue for Qt6 with MXE: Can't deduce `auto*`
    Because of different iterator types in Qt6, we can't use `auto*` for
    `std::find` on actor lists.  clang-tidy always auto-corrects it, but
    it's wrong.

--- src/data/Actor.cpp
+++ src/data/Actor.cpp
@@ -26,7 +26,8 @@ void Actors::addActor(Actor actor)
 
 void Actors::removeActor(Actor* actor)
 {
-    auto* i = std::find(m_actors.begin(), m_actors.end(), actor);
+    // Note on clang-tidy: We can't use `auto*` for Qt6
+    auto i = std::find(m_actors.begin(), m_actors.end(), actor);
     if (i != m_actors.end()) {
         m_actors.erase(i);
         delete actor;
