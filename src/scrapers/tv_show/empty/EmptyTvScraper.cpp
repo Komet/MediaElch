ad8ec029fbd0416dddad3e950b54af6003de0433
Andre Meyering <info@andremeyering.de>
2023-05-25 17:52:30 +0200

    refactor: Add further example TV show scraper main class

--- src/scrapers/tv_show/empty/EmptyTvScraper.cpp
+++ src/scrapers/tv_show/empty/EmptyTvScraper.cpp
@@ -5,6 +5,62 @@
 namespace mediaelch {
 namespace scraper {
 
+QString EmptyTv::ID = "empty";
+
+EmptyTv::EmptyTv(QObject* parent) : TvScraper(parent)
+{
+    m_meta.identifier = EmptyTv::ID;
+    m_meta.name = "Empty TV";
+    m_meta.description = "placeholder";
+    m_meta.website = "https://www.example.com";
+    m_meta.termsOfService = "https://www.example.com/terms";
+    m_meta.privacyPolicy = "https://www.example.com/privacy";
+    m_meta.help = "https://help.example.com";
+    // or e.g. mediaelch::allShowScraperInfos();
+    m_meta.supportedShowDetails = {ShowScraperInfo::Title};
+    // or e.g. mediaelch::allEpisodeScraperInfos();
+    m_meta.supportedEpisodeDetails = {EpisodeScraperInfo::Title};
+
+    m_meta.supportedSeasonOrders = {SeasonOrder::Aired};
+    m_meta.supportedLanguages = {Locale::English};
+    m_meta.defaultLocale = Locale::English;
+}
+
+const EmptyTv::ScraperMeta& EmptyTv::meta() const
+{
+    return m_meta;
+}
+
+void EmptyTv::initialize()
+{
+    QTimer::singleShot(0, this, [this]() { emit initialized(true, this); });
+}
+
+bool EmptyTv::isInitialized() const
+{
+    return true;
+}
+
+ShowSearchJob* EmptyTv::search(ShowSearchJob::Config config)
+{
+    return new EmptyShowSearchJob(config, this);
+}
+
+ShowScrapeJob* EmptyTv::loadShow(ShowScrapeJob::Config config)
+{
+    return new EmptyShowScrapeJob(config, this);
+}
+
+SeasonScrapeJob* EmptyTv::loadSeasons(SeasonScrapeJob::Config config)
+{
+    return new EmptySeasonScrapeJob(config, this);
+}
+
+EpisodeScrapeJob* EmptyTv::loadEpisode(EpisodeScrapeJob::Config config)
+{
+    return new EmptyEpisodeScrapeJob(config, this);
+}
+
 EmptyShowSearchJob::EmptyShowSearchJob(ShowSearchJob::Config _config, QObject* parent) : ShowSearchJob(_config, parent)
 {
 }
--- src/scrapers/tv_show/empty/EmptyTvScraper.h
+++ src/scrapers/tv_show/empty/EmptyTvScraper.h
@@ -5,6 +5,32 @@
 namespace mediaelch {
 namespace scraper {
 
+class EmptyTv : public TvScraper
+{
+    Q_OBJECT
+
+public:
+    static QString ID;
+
+public:
+    explicit EmptyTv(QObject* parent = nullptr);
+    ~EmptyTv() override = default;
+
+    const ScraperMeta& meta() const override;
+
+    void initialize() override;
+    bool isInitialized() const override;
+
+    ShowSearchJob* search(ShowSearchJob::Config config) override;
+    ShowScrapeJob* loadShow(ShowScrapeJob::Config config) override;
+    SeasonScrapeJob* loadSeasons(SeasonScrapeJob::Config config) override;
+    EpisodeScrapeJob* loadEpisode(EpisodeScrapeJob::Config config) override;
+
+private:
+    ScraperMeta m_meta;
+};
+
+
 /// \brief   Empty show search job for testing purposes.
 /// \details This class can be used as a placeholder for new scrapers that did
 ///          not yet implement certain features.
