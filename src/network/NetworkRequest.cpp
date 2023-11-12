48d2033d24d14868b1f38d9048c9e3788a12e3b2
Andre Meyering <info@andremeyering.de>
2023-11-12 18:43:44 +0100

    chore(Network): Update Firefox user agent

--- src/network/NetworkRequest.cpp
+++ src/network/NetworkRequest.cpp
@@ -36,7 +36,7 @@ QNetworkRequest jsonRequestWithDefaults(const QUrl& url)
 void useFirefoxUserAgent(QNetworkRequest& request)
 {
     request.setHeader(QNetworkRequest::UserAgentHeader,
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0");
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0");
 }
 
 } // namespace network
