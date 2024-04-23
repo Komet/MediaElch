dca866aa3bdb31f31856ebac642544be0e6091b9
Andre Meyering <info@andremeyering.de>
2024-04-23 19:53:00 +0200

    fix(macOS): Support building and running on macOS 10.13 again
    We don't support darkmode on macOS 10.x anymore.  Users can use our Qt6
    build instead.

--- src/ui/MacUiUtilities.mm
+++ src/ui/MacUiUtilities.mm
@@ -2,19 +2,27 @@
 
 #undef slots
 #import <Cocoa/Cocoa.h>
+#include <QtVersionChecks>
 
 namespace mediaelch {
 namespace ui {
 
 bool macIsInDarkTheme()
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    // macOS 10.13 doesn't support NSAppearanceNameDarkAqua and we can't build it on it.
+    // The easiest way I found was to just drop support for dark mode on macOS 10.X.
+    // With macOS 11 and later, users can use our Qt6 version.
+    return false;
+#else
     // See tutorial at <https://successfulsoftware.net/2021/03/31/how-to-add-a-dark-theme-to-your-qt-application/>
     if (__builtin_available(macOS 10.14, *)) {
         auto appearance = [NSApp.effectiveAppearance
-            bestMatchFromAppearancesWithNames:@[NSAppearanceNameAqua, NSAppearanceNameDarkAqua]];
+            bestMatchFromAppearancesWithNames:@[ NSAppearanceNameAqua, NSAppearanceNameDarkAqua ]];
         return [appearance isEqualToString:NSAppearanceNameDarkAqua];
     }
     return false;
+#endif
 }
 
 } // namespace ui
