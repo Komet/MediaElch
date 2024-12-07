2cc105780ed5c72db558b110154fd8ef83d4a8e1
Andre Meyering <info@andremeyering.de>
2024-12-07 14:22:57 +0100

    chore(Helper): Add no-discard attribute to helper functions

--- src/globals/Helper.h
+++ src/globals/Helper.h
@@ -11,23 +11,23 @@
 
 namespace helper {
 
-QString toLatin1PercentEncoding(QString str);
-QString urlDecode(QString str);
-QString urlEncode(QString str);
-QString formatTrailerUrl(QString url);
+ELCH_NODISCARD QString toLatin1PercentEncoding(QString str);
+ELCH_NODISCARD QString urlDecode(QString str);
+ELCH_NODISCARD QString urlEncode(QString str);
+ELCH_NODISCARD QString formatTrailerUrl(QString url);
 
 /// \brief Returns the index for the short English month name (Jan = 1, Dec = 12);
 /// Returns -1 for unknown one.
 /// This function can be replaced by QDate::fromString(str, "MMM") in Qt 6,
 /// but in Qt 5, that function is locale aware.
-int monthNameToInt(const QString& month);
+ELCH_NODISCARD int monthNameToInt(const QString& month);
 
-bool isDvd(const mediaelch::DirectoryPath& path, bool noSubFolder = false);
-bool isDvd(const mediaelch::FilePath& path, bool noSubFolder = false);
-bool isDvd(QString path, bool noSubFolder = false);
-bool isBluRay(const mediaelch::DirectoryPath& path);
-bool isBluRay(const mediaelch::FilePath& path);
-bool isBluRay(QString path);
+ELCH_NODISCARD bool isDvd(const mediaelch::DirectoryPath& path, bool noSubFolder = false);
+ELCH_NODISCARD bool isDvd(const mediaelch::FilePath& path, bool noSubFolder = false);
+ELCH_NODISCARD bool isDvd(QString path, bool noSubFolder = false);
+ELCH_NODISCARD bool isBluRay(const mediaelch::DirectoryPath& path);
+ELCH_NODISCARD bool isBluRay(const mediaelch::FilePath& path);
+ELCH_NODISCARD bool isBluRay(QString path);
 
 /**
  * Sanitize the given file name by replacing characters commonly not supported by filesystems
@@ -39,30 +39,30 @@ void sanitizeFileName(QString& fileName, const QString& defaultDelimiter = " ");
  * with a space or the given `defaultDelimiter`.
  */
 void sanitizeFolderName(QString& fileName, const QString& defaultDelimiter = " ");
-QString appendArticle(const QString& text);
+ELCH_NODISCARD QString appendArticle(const QString& text);
 
-QString mapGenre(const QString& text);
-QStringList mapGenre(const QStringList& genres);
-Certification mapCertification(const Certification& certification);
-QString mapStudio(const QString& text);
-QStringList mapStudio(const QStringList& studio);
-QString mapCountry(const QString& text);
-QStringList mapCountry(const QStringList& countries);
+ELCH_NODISCARD QString mapGenre(const QString& text);
+ELCH_NODISCARD QStringList mapGenre(const QStringList& genres);
+ELCH_NODISCARD Certification mapCertification(const Certification& certification);
+ELCH_NODISCARD QString mapStudio(const QString& text);
+ELCH_NODISCARD QStringList mapStudio(const QStringList& studio);
+ELCH_NODISCARD QString mapCountry(const QString& text);
+ELCH_NODISCARD QStringList mapCountry(const QStringList& countries);
 
-QString formatFileSizeBinary(double size, const QLocale& locale);
-QString formatFileSize(double size, const QLocale& locale);
-QString formatFileSize(int64_t size, const QLocale& locale);
+ELCH_NODISCARD QString formatFileSizeBinary(double size, const QLocale& locale);
+ELCH_NODISCARD QString formatFileSize(double size, const QLocale& locale);
+ELCH_NODISCARD QString formatFileSize(int64_t size, const QLocale& locale);
 
-qreal similarity(const QString& s1, const QString& s2);
-QMap<ColorLabel, QString> labels();
-QColor colorForLabel(ColorLabel label, QString theme);
-QIcon iconForLabel(ColorLabel label);
-QString matchResolution(int width, int height, const QString& scanType);
+ELCH_NODISCARD qreal similarity(const QString& s1, const QString& s2);
+ELCH_NODISCARD QMap<ColorLabel, QString> labels();
+ELCH_NODISCARD QColor colorForLabel(ColorLabel label, QString theme);
+ELCH_NODISCARD QIcon iconForLabel(ColorLabel label);
+ELCH_NODISCARD QString matchResolution(int width, int height, const QString& scanType);
 
 /// \brief Take the given URL and make an HTML link tag.
-QString makeHtmlLink(const QUrl& url);
+ELCH_NODISCARD QString makeHtmlLink(const QUrl& url);
 
 // String Utils
-bool containsIgnoreCase(const QStringList& list, const QString& compare);
+ELCH_NODISCARD bool containsIgnoreCase(const QStringList& list, const QString& compare);
 
 } // namespace helper
